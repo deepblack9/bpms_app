d58cc3772d4d30b8e949b3fa87443b0a9c8cd172
0427志强 <goujiazhuangzz@163.com>
2019-06-12 16:43:07 +0800

    更新“综合监管”界面

--- lib/page/supervision_page.dart
+++ lib/page/supervision_page.dart
@@ -12,16 +12,103 @@ class _State extends State<SupervisionPage> {
 
   @override
   void initState() {
-    headerList.add("1");
-    headerList.add("2");
-    headerList.add("3");
-    headerList.add("4");
-    headerList.add("5");
-    headerList.add("6");
+    headerList.add("企业1");
+    headerList.add("企业2");
+    headerList.add("企业3");
+    headerList.add("企业4");
+    headerList.add("企业5");
+    headerList.add("企业6");
   }
 
   @override
   Widget build(BuildContext context) {
+    /// 企业信息
+    Widget buildComponyRow(String text) {
+      return Container(
+        padding: const EdgeInsets.all(5.0),
+        child:  Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: 160,
+              alignment: FractionalOffset.center,
+              padding: const EdgeInsets.only(bottom: 8.0),
+              child: new Text(text,
+                style: TextStyle(
+//                  inherit: true,
+//                  color: Colors.white,
+                  fontSize: 30.0,
+//                  fontWeight: FontWeight.w400,
+//                  fontStyle: FontStyle.italic,
+//                  letterSpacing: 5,
+//                  wordSpacing: 20,
+//                  textBaseline: TextBaseline.alphabetic,
+//                  height: 1.2,
+//                  locale: Locale('fr', 'CH'),
+//                  background: Paint() ..color = Colors.blue,
+//                  shadows: [Shadow(color:Colors.black,offset: Offset(6, 3), blurRadius: 10)],
+//                  decoration: TextDecoration.underline,
+//                  decorationColor: Colors.black54,
+//                  decorationStyle: TextDecorationStyle.dashed,
+//                  debugLabel: 'test',
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: new Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: new Text('融资金额1200万元'),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: new Text('当前货值评估948万元'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    /// 仓库信息
+    Widget buildWarehouseRow() {
+      return new ListView.builder(
+        shrinkWrap: true,
+        itemCount: 1,
+        itemBuilder: (context, index) {
+          return new Container(
+            padding: const EdgeInsets.all(5.0),
+            child: new Row(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Expanded(
+                  child: new Container(
+                    alignment: FractionalOffset.center,
+                    padding: const EdgeInsets.all(8.0),
+                    child: new Text("中心仓库"),
+                  ),
+                ),
+                Expanded(
+                  child: new Container(
+                    alignment: FractionalOffset.center,
+                    padding: const EdgeInsets.all(8.0),
+                    child: new Text("估值948万元"),
+                  ),
+                ),
+              ],
+            )
+          );
+        },
+      );
+    }
+
     return Container(
       margin: new EdgeInsets.only(left: 3.0, right: 3.0),
       child: new ListView.builder(
@@ -29,29 +116,22 @@ class _State extends State<SupervisionPage> {
         itemCount: headerList.length,
         itemBuilder: (context, index) {
           return new Container(
-            child: new Row(
-              children: <Widget>[
-                new Row(
+            padding: const EdgeInsets.all(5.0),
+            child: Card(
+              child: new Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
                   children: <Widget>[
-
+                    buildComponyRow('${headerList[index]}'),
+                    Divider(
+                      color: Colors.grey,
+                      height: 5.0,
+                      //左边缩进
+                      // indent: 32.0,
+                    ),
+                    buildWarehouseRow(),
                   ],
                 ),
-                new Row(
-
-                ),
-                new Row(
-                  children: <Widget>[
-                    new Column(
-                    children: <Widget>[
-
-                      ]
-                    ),
-                    new Column(
-
-                    ),
-                  ],
-                ),
-              ],
             )
           );
 //          return new ListTile(
@@ -68,7 +148,6 @@ class _State extends State<SupervisionPage> {
         },
       ),);
   }
-
 
 //  @override
 //  requestRefresh() async {
